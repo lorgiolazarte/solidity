2aaf54a4734450186a6029ade87638113580c249
lorgiolazarte <lorgi51@gmail.com>
2024-10-16 10:30:30 -0400

    contratos adicionales

---
+++ hello_dinamic.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract hello_dinamic {
+    string saludo_d = "Saludo Dinamico";
+    string public saludo_e = "Saludo inicial despliegue contrato - estatico";
+
+    function leerSaludo () public view returns (string memory ) {
+        return saludo_d;
+    }
+
+    function guardarSaludo(string memory _nuevoSaludo) public  {
+        saludo_d = _nuevoSaludo;
+    }
+}
