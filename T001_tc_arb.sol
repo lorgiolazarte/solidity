5d58c97b43667fbad1cf2fc4c3954baba8a23da7
lorgiolazarte <lorgi51@gmail.com>
2024-10-18 11:44:03 -0400

    004

--- T001_tc_arb.sol
+++ T001_tc_arb.sol
@@ -27,63 +27,64 @@ string cel_number,
 int8 status,
 address wallet);
 
-// modifier para que solo el ownwr pueda retirar los fondos del contratoi
-modifier onlyOwner(){
-require(msg.sender == owner, "Solo el propietario puede retirar los fondos");
-_;
-}
+    // modifier para que solo el ownwr pueda retirar los fondos del contratoi
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Solo el propietario puede retirar los fondos");
+        _;
+    }
 
-/*
-modifier lo que hace es verificar dentro de otras funciones que al ejecutar
-la funcio no se tenga el valor del fee + el gas
-*/
-modifier cost(uint amount) {
-require(msg.value >= amount, "No tiene saldo en ARB para ejcutar");
-_;
-}
+    /*
+    modifier lo que hace es verificar dentro de otras funciones que al ejecutar
+    la funcio no se tenga el valor del fee + el gas
+    */
+    modifier cost(uint amount) {
+        require(msg.value >= amount, "No tiene saldo en ARB para ejcutar");
+        _;
+    }
 
-// se define quien es el propietario del contrato
-constructor(){
-owner = payable(msg.sender);
-}
+    // se define quien es el propietario del contrato
+    constructor(){
+        owner = payable(msg.sender);
+    }
 
-// funcion para cambiar los datos del enum
-function pushData(string memory _date,
-string memory _delivery_date,
-string memory _product,
-string memory _price,
-string memory _code,
-string memory _cel_number,
-int8 _status) public payable cost(1000000000000000){
-data = Data(_date,
-_delivery_date,
-_product,
-_price,
-_code,
-_cel_number,
-_status,
-msg.sender);
+    // funcion para cambiar los datos del enum
+    function pushData(string memory _date,
+    string memory _delivery_date,
+    string memory _product,
+    string memory _price,
+    string memory _code,
+    string memory _cel_number,
+    int8 _status) public payable cost(1000000000000000){
+        data = Data(_date,
+        _delivery_date,
+        _product,
+        _price,
+        _code,
+        _cel_number,
+        _status,
+        msg.sender);
 
-// es como una especie de log
-emit newData(_date,
-_delivery_date,
-_product,
-_price,
-_code,
-_cel_number,
-_status,
-msg.sender);
-}
+        // es como una especie de log
+        emit newData(_date,
+        _delivery_date,
+        _product,
+        _price,
+        _code,
+        _cel_number,
+        _status,
+        msg.sender);
+    }
 
-// funcion para retirar los fondos del contrato
-function withdraw() public onlyOwner {
-uint balance = address(this).balance;
-require(balance > 0, "No se tienen fondos en el contrato todavia");
-owner.transfer(balance);
-}
+    // funcion para retirar los fondos del contrato
+    function withdraw() public onlyOwner {
+        uint balance = address(this).balance;
+        require(balance > 0, "No se tienen fondos en el contrato todavia");
+        owner.transfer(balance);
+    }
 
-// funcion para consultar el valor que se encuentra en el contrato
-function getBalance() public view returns (uint256) {
-return address(this).balance;
-}
+    // funcion para consultar el valor que se encuentra en el contrato
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
 }
